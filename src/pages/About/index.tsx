98d08f65762e0e883beccb0b028fd8eb1a29e332
ommiputera <omiputrakarunia@gmail.com>
2023-01-24 13:58:31 +0700

    scroll to top

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,9 +1,14 @@
+import React from 'react'
 import {Helmet} from 'react-helmet'
 import styled from 'styled-components'
 import {HeroBackgrounds} from '../../layouts/Backgrounds'
 import HeroSection from './HeroSection'
 
 function About() {
+  React.useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
   return (
     <Wrapper>
       <AboutHelmet />
