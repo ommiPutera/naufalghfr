25c81a179bfcd270a1352450b833361ac8677c81
Product Design <naufalghfr@Indofunds-MacBook-Pro-3.local>
2023-04-03 01:31:26 +0700

    test ganti ke google

--- src/pages/Home/props/index.tsx
+++ src/pages/Home/props/index.tsx
@@ -45,7 +45,7 @@ const propsContentProjects: CardItemTypes[] = [
   {
     assetSrc: '/assets/image/previewProjects/kenari-admin-site.jpg',
     title: 'Kenari Admin Site',
-    to: 'https://naufalghfr.notion.site/Designing-an-Admin-Site-Dashboard-for-Kenari-Apps-638929677483419c83edae80df19e040',
+    to: 'www.google.com',
     desc: (
       <span>
         Designing an back-end system for the <br /> Kenari Apps
